95d0fa99af74a6e5f1a847eaad9176a465e68d1b
licheng1013 <2501093733@qq.com>
2023-07-04 17:42:50 +0800

    增加java语言支持!

---
+++ java-admin/READE.md
@@ -0,0 +1,7 @@
+# Java-Admin
+## 项目介绍
+- 适用于后台管理系统
+
+## 配套插件
+- https://plugins.jetbrains.com/plugin/20888-javaorm
+- 支持后端代码生成,暂时不支持前端代码生成
---
+++ java-admin/admin/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lc
+ * @since 2023/7/4
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
