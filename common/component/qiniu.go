8c3c0fcda15f9d9e379ab6e866e6a943f7a047d9
licheng <2501093733@qq.com>
2023-07-21 16:57:21 +0800

    增加七牛云组件!

---
+++ common/component/qiniu.go
@@ -0,0 +1,57 @@
+package component
+
+import (
+	"github.com/qiniu/go-sdk/v7/auth/qbox"
+	"github.com/qiniu/go-sdk/v7/storage"
+	"log"
+	"strings"
+)
+
+// 七牛云密钥 - 密钥查看: https://portal.qiniu.com/user/key
+const accessKey = "your accessKey"
+const secretKey = "your secretKey"
+
+// 你的资源资源桶名称 - 桶查看: https://portal.qiniu.com/kodo/bucket
+const bucket = "my-video-test"
+
+// 获取token
+func getToken() {
+	putPolicy := storage.PutPolicy{
+		Scope: bucket,
+	}
+	mac := qbox.NewMac(accessKey, secretKey)
+	upToken := putPolicy.UploadToken(mac)
+	log.Println(upToken)
+}
+
+// DeleteFile 删除文件
+func DeleteFile(key string) error {
+	mac := qbox.NewMac(accessKey, secretKey)
+	cfg := storage.Config{
+		UseHTTPS: false,
+		Zone:     &storage.ZoneHuanan,
+	}
+	bucketManager := storage.NewBucketManager(mac, &cfg)
+	err := bucketManager.Delete(bucket, key)
+	return err
+}
+
+// DeleteFileByUrl 根据url删除
+func DeleteFileByUrl(url string) error {
+	key := ParseKey(url)
+	return DeleteFile(key)
+}
+
+// ParseKey 解析 url中的key
+func ParseKey(url string) string {
+	// 从url中解析出key
+	index := strings.LastIndex(url, "/")
+	key := url[index+1:]
+
+	// 去除参数
+	index = strings.Index(key, "?")
+	if index != -1 {
+		key = key[:index]
+	}
+	return key
+}
--- common/go.mod
+++ common/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gin-gonic/gin v1.9.1
 	github.com/golang-jwt/jwt/v5 v5.0.0
 	github.com/google/uuid v1.3.0
+	github.com/qiniu/go-sdk/v7 v7.17.0
 	gopkg.in/yaml.v3 v3.0.1
 	gorm.io/driver/mysql v1.5.1
 	gorm.io/gorm v1.25.1
@@ -23,6 +24,7 @@ require (
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -36,8 +38,8 @@ require (
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
-	golang.org/x/exp v0.0.0-20221208152030-732eee02a75a // indirect
 	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
